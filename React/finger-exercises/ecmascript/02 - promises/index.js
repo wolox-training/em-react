8773a84bc1b3a72238e7022d4c53698bf951f2e6
Enrique Mejia <enriquemejia@Woloxs-MacBook-Pro.local>
2018-07-19 11:30:17 -0500

    Completed promises tests

--- React/finger-exercises/ecmascript/02 - promises/index.js
+++ React/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,9 +1,16 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  const start = Date.now();
+  return new Promise((resolve, reject) => {
+    if (time > (1000 * 60)) reject(new Error('This time is too much !'));
+    setTimeout(() => {
+      resolve(Date.now() - start);
+    }, time);
+  });
 }
 
-export function asyncDelay() {
-
+export async function asyncDelay(time) {
+  const start = Date.now();
+  return new Promise((resolve) => setTimeout(() => resolve(Date.now() - start), time));
 }
