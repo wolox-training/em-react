d7ab31fd8e26480e9c036c84bf4099aedf8542ac
Enrique Mejia <enriquemejia@Woloxs-MacBook-Pro.local>
2018-07-19 09:52:56 -0500

    Passed min tests

--- React/finger-exercises/ecmascript/01 - spread/index.js
+++ React/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,8 +1,17 @@
 import { isArray } from './utils';
 
-export function min() {
-  isArray();
-  return Math.min();
+export function min(...args) {
+  if (!args.length) return undefined;
+  if (args.length === 1 && !isArray(args[0])) return args[0];
+
+  const newArgs = [];
+
+  args.forEach(arg => {
+    if (isArray(arg)) newArgs.push(...arg);
+    else newArgs.push(arg);
+  });
+
+  return Math.min(...newArgs);
 }
 
 export function copy() {
