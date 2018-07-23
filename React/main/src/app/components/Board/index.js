23924d2dbee370fee39c3855946e38b9197ac908
Enrique Mejia <enrique.mejia@wolox.co>
2018-07-23 09:31:02 -0500

    Created a new property in the state of the Board component to know if the next move should be X or O

--- React/main/src/app/components/Board/index.js
+++ React/main/src/app/components/Board/index.js
@@ -6,13 +6,17 @@ import style from './styles.scss';
 
 class Board extends Component {
   state = {
-    squares: Array(9).fill(null)
+    squares: Array(9).fill(null),
+    xIsNext: true
   };
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare(i) {
@@ -20,7 +24,7 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
