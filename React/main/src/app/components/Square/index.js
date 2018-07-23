6cfb811952ef96f235cc20e438b54b8364227ddb
Enrique Mejia <enriquemejia@Woloxs-MacBook-Pro-2.local>
2018-07-23 09:05:14 -0500

    Created an event handler for the click event on the Square component to display an alert on the browser

--- React/main/src/app/components/Square/index.js
+++ React/main/src/app/components/Square/index.js
@@ -5,7 +5,7 @@ import style from './styles.scss';
 class Square extends Component {
   render() {
     return (
-      <button className={style.square}>
+      <button className={style.square} onClick={() => {alert('click')}}>
         { this.props.value }
       </button>
     );
