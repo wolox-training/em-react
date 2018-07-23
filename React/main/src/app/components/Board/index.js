bdacf485942945fcd383d32efa9526a2c75bcc9c
Enrique Mejia <enriquemejia@Woloxs-MacBook-Pro-2.local>
2018-07-23 09:21:10 -0500

    Changed the onClick event on the Square component to lift state up on the Board Component in order to handle its value via Props. Also defined the PropTypes for the Square component

--- React/main/src/app/components/Board/index.js
+++ React/main/src/app/components/Board/index.js
@@ -5,8 +5,18 @@ import Square from '~components/Square';
 import style from './styles.scss';
 
 class Board extends Component {
+  state = {
+    squares: Array(9).fill(null)
+  };
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
--- React/main/src/app/components/Square/index.js
+++ React/main/src/app/components/Square/index.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import style from './styles.scss';
 
 class Square extends Component {
-
-  state = {
-    value: null
-  };
-
   render() {
     return (
-      <button className={style.square} onClick={() => this.setState({ value: 'X' })}>
-        {this.state.value}
+      <button className={style.square} onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
+
+Square.propTypes = {
+  onClick: PropTypes.func,
+  value: PropTypes.string
+};
 
 export default Square;
